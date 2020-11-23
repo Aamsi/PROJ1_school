c5bd12fb67c10668382ee761c6d1ed534a5d12a1
Aamsi <ismaelouali45@gmail.com>
2020-11-23 18:08:26 +0100

    gdfsdsrg

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: iouali <iouali@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 10:24:48 by iouali            #+#    #+#             */
-/*   Updated: 2020/11/23 11:32:10 by iouali           ###   ########.fr       */
+/*   Updated: 2020/11/23 11:35:49 by iouali           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ unsigned int	ft_strlcat(char *dest, const char *src, unsigned int size)
 {
 	size_t			i;
 	size_t			j;
-	unsigned int	destlen;
+	unsigned int	len_dest;
 	unsigned char	*tmp_src;
 
 	tmp_src = (unsigned char *)src;
@@ -26,7 +26,7 @@ unsigned int	ft_strlcat(char *dest, const char *src, unsigned int size)
 	while (dest[i])
 		i++;
 	j = 0;
-	destlen = i;
+	len_dest = i;
 	while (j + i < size - 1 && tmp_src[j])
 	{
 		dest[i + j] = tmp_src[j];
@@ -34,7 +34,7 @@ unsigned int	ft_strlcat(char *dest, const char *src, unsigned int size)
 	}
 	if (j < size)
 		dest[i + j] = '\0';
-	if (size < destlen)
+	if (size < len_dest)
 		return (ft_strlen(src) + size);
-	return (destlen + ft_strlen(src));
+	return (len_dest + ft_strlen(src));
 }
